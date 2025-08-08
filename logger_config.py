f443d4dafc9aa035fdf17634b4e3f82c208a741a
mhovhannisyan <mhovhannisyan@trading-point.com>
2025-08-08 19:23:18 +0400

    homework 12 logger logic

--- logger_config.py
+++ logger_config.py
@@ -2,21 +2,22 @@ import logging
 
 # Create logger
 logger = logging.getLogger("my_logger")
-logger.setLevel(logging.DEBUG)
+if not logger.handlers:
+    logger.setLevel(logging.DEBUG)
 
-# Create handler for console
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.WARNING)
+    # Create handler for console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.WARNING)
 
-# Create handler for writing in a file
-file_handler = logging.FileHandler('app.log')
-file_handler.setLevel(logging.DEBUG)
+    # Create handler for writing in a file
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setLevel(logging.DEBUG)
 
-# Create formatter
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
+    # Create formatter
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
-# Add handlers to logger
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+    # Add handlers to logger
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
